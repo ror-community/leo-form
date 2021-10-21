43e557bc30a08c9fc527b2ad93655cceede0113e
Esha Datta <edatta@crossref.org>
2021-10-21 11:31:27 -0400

    making changes for rorschema app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <template>
   <!-- App.vue -->
 
-  <v-app :style="{background: backgroundColour}">
+  <v-app :style="{ 'background-color': 'white'}">
     <v-main>
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
-        <v-row class="mt-1">
-          <v-col cols="12">
-            <v-card>
-              <v-card-text class="pa-6">
-                <v-btn color="primary" @click="currentComponent='ExampleJsonForms'">JSON Forms example</v-btn>
-                <v-btn class="ml-4" outlined color="primary" @click="currentComponent='ExampleComponent'">Toggle component exmaple</v-btn>
-              </v-card-text>
-            </v-card>
-          </v-col>
-        </v-row>
         <v-row>
           <v-col cols="12">
             <v-card elevation="0">
@@ -53,15 +43,8 @@ export default defineComponent({
   name: 'App',
   components: { ExampleComponent, ExampleJsonForms, VCard, VBtn },
   setup () {
-    const toggleService = provideToggleService()
-    const toggleMachine = useActor(toggleService)
-    // Declare a computed value
-    const backgroundColour = computed(() => {
-      return toggleMachine.state.value.value === 'active' ? colors.blue.lighten3 : colors.pink.lighten3
-    })
-    const components = ['ExampleComponent', 'ExampleJsonForms']
-    const currentComponent = components[1]
-    return { backgroundColour: backgroundColour, currentComponent }
+    const currentComponent = 'ExampleJsonForms'
+    return { currentComponent }
   },
   data: () => ({
     // TODO: wire up theme choice to $vuetify property
