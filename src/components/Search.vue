13150749e644e5e1fd83de69c7bcddc55a187a10
Esha Datta <edatta@crossref.org>
2021-11-04 08:49:03 -0400

    autocomplete

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,7 +2,7 @@
         <v-autocomplete
           :items="entries"
           :loading="isLoading"
-          :search-input.sync="search"
+          search-input="fetchROR"
           color="white"
           hide-no-data
           hide-selected
@@ -16,37 +16,18 @@
 </template>
 
 <script>
-
 export default {
-  data () {
-   return {
-    entries: [],
-    isLoading: false,
-    search: null,
+   data() {
+    return {
+      entries: [],
+      isLoading: false,
+      search: null,
     }
   },
-
-  watch: {
-    search (val) {
+  methods: {
+    fetchROR(val) {
       console.log('VAL: ', val)
-      // Items have already been loaded
-      if (this.entries.length > 0) return
-
-      // Items have already been requested
-      if (this.isLoading) return
-
-      this.isLoading = true
-
-      // Lazily load input items
-      fetch(`https://api.ror.org/organizations?query=${val}`)
-        .then(res => res.json())
-        .then(res => {
-          this.entries = res.items
-        })
-        .catch(err => {
-          console.log(err)
-        })
-        .finally(() => (this.isLoading = false))
-    },
+    }
   }
 }
+</script>
