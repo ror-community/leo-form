f087b2bbb3fb318046f3ad82ef3d9426c9b9e490
lizkrznarich <liz.krznarich@datacite.org>
2022-08-29 11:06:43 -0500

    copy env file during dev deploy

---
+++ src/env.dev.ts
@@ -0,0 +1,9 @@
+export const env = () => {
+  const {
+    VUE_APP_BASE_ROUTE: BASE_ROUTE = '/',
+    VUE_APP_GENERATE_ID: GENERATE_ID = 'https://generateid.dev.ror.org/generateid',
+    VUE_APP_ADDRESS_URL: ADDRESS_URL = 'https://generateid.dev.ror.org/address',
+    NODE_ENV
+  } = process.env
+  return { BASE_ROUTE, GENERATE_ID, ADDRESS_URL, NODE_ENV }
+}
